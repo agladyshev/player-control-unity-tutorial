580e952a188f63c5aded4cbea2e0adca548aab53
Alex Gladyshev <alex@lifter.ai>
2023-01-28 11:13:29 +0400

    Add user controls to move vehicle forward and steer

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5.0f;    
+    private float speed = 5.0f;
+    private float turnSpeed = 25;
+    private float horizontalInput;
+    private float forwardInput;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,8 +18,15 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
         //forward moves object 1 metre
         //deltaTime is second / number of frames per second 
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        if (forwardInput != 0)
+        {
+            // If the vehicle is moving, allow steering 
+            transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+        } 
     }
 }
